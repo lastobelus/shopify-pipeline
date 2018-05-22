cbb3de9ca093268851ee7765a81ab8f0821bfd75
Michael Johnston <lastobelus@mac.com>
2018-05-22 15:30:19 -0700

    turn on uglify warnings

--- config/webpack.prod.conf.js
+++ config/webpack.prod.conf.js
@@ -81,7 +81,7 @@ module.exports = merge(webpackConfig, {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
+        warnings: true
       }
     }),
 
